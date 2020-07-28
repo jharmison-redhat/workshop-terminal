80a47a57504bff26ae9a2a3034806fbcb68407e2
James Harmison <jharmison@redhat.com>
2020-07-28 19:46:55 -0400

    Bumped a bunch of versions

--- hack/build.sh
+++ hack/build.sh
@@ -5,7 +5,7 @@ CONTAINER_IMAGE=workshop-terminal
 DOCKERFILE_DIR=terminal
 
 function print_usage() {
-  echo "usage: $0 [-l (local|quay)] [-p QUAY_PROJECT] [-- BUILD_ARGS]"
+  echo "usage: $0 [-l (local|quay)] [-p QUAY_PROJECT] [-t CONTAINER_TAG] [-- BUILD_ARGS]"
 }
 
 # parse args
@@ -25,6 +25,14 @@ while [ $# -gt 0 ]; do
         QUAY_PROJECT="$1"
       else
         QUAY_PROJECT=$(echo "$1" | cut -d= -f2-)
+      fi
+      ;;
+    -t|--tag=*)
+      if [ "$1" = '-t' ]; then
+        shift
+        CONTAINER_TAG="$1"
+      else
+        CONTAINER_TAG=$(echo "$1" | cut -d= -f2-)
       fi
       ;;
     --)
@@ -50,6 +58,9 @@ fi
 if [ -z "$QUAY_PROJECT" ]; then
   QUAY_PROJECT=redhatgov
 fi
+if [ -z "$CONTAINER_TAG" ]; then
+  CONTAINER_TAG=latest
+fi
 
 # docker/podman problems
 if ! which docker &>/dev/null; then
@@ -64,7 +75,7 @@ fi
 # build
 case $LOCATION in
   local)
-    docker build "${@}" -t quay.io/$QUAY_PROJECT/$CONTAINER_IMAGE:latest .
+    docker build "${@}" -t quay.io/$QUAY_PROJECT/$CONTAINER_IMAGE:$CONTAINER_TAG .
   ;;
   quay)
     # designed to be used by travis-ci, where the docker_* variables are defined
@@ -74,8 +85,8 @@ case $LOCATION in
     fi
     echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin quay.io || exit 2
 
-    docker build "${@}" -t quay.io/$QUAY_PROJECT/$CONTAINER_IMAGE:latest . || exit 3
-    docker push quay.io/$QUAY_PROJECT/$CONTAINER_IMAGE:latest || exit 4
+    docker build "${@}" -t quay.io/$QUAY_PROJECT/$CONTAINER_IMAGE:$CONTAINER_TAG . || exit 3
+    docker push quay.io/$QUAY_PROJECT/$CONTAINER_IMAGE:$CONTAINER_TAG || exit 4
   ;;
   *)
     print_usage >&2
